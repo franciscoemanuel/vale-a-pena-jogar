fe055e28cca6789a518daf6e5aec92d480dddda3
EikeSan <eikesantz@hotmail.com>
2016-10-19 15:25:29 -0300

    att branch

--- public/index.php
+++ public/index.php
@@ -1,85 +1,58 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="favicon.ico">
+<?php
 
-    <title>VAPJ</title>
-    
-    <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylor@laravel.com>
+ */
 
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| our application. We just need to utilize it! We'll simply require it
+| into the script here so that we don't have to worry about manual
+| loading any of our classes later on. It feels nice to relax.
+|
+*/
 
-    <!-- Personal CSS -->
-    <link href="css/main.css" rel="stylesheet" type="text/css">
+require __DIR__.'/../bootstrap/autoload.php';
 
-    <!-- Custom styles for this template -->
-    <!-- <link href="jumbotron.css" rel="stylesheet"> -->
+/*
+|--------------------------------------------------------------------------
+| Turn On The Lights
+|--------------------------------------------------------------------------
+|
+| We need to illuminate PHP development, so let us turn on the lights.
+| This bootstraps the framework and gets it ready for use, then it
+| will load up this application so that we can run it and send
+| the responses back to the browser and delight our users.
+|
+*/
 
-    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="js/ie-emulation-modes-warning.js"></script>
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request
+| through the kernel, and send the associated response back to
+| the client's browser allowing them to enjoy the creative
+| and wonderful application we have prepared for them.
+|
+*/
 
-  <body>
-    <nav class="navbar navbar-default borderLinha navPrincipal ">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <a href="#">
-           <img class="logoVA" alt="Brand" src="img/logo2.png">
-          </a>
-        </div>
-        <div>
-          <p class="navbar-text navbar-right linkMenu">
-            <a href="#" class="navbar-link">Login</a> | <a href="#" class="navbar-link">Cadastre-se</a>
-          </p>
-        </div>
-      </div>
-    </nav>
-    <nav class="navbar navbar-default navPrincipal" >
-      <div class="container-fluid barraMenu">
-      <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Inicio <span class="sr-only">(current)</span></a></li>
-            <li><a href="#">Jogos</a></li>
-            <li><a href="#">Listas</a></li>
-            <li><a href="#">Usuários</a></li>
-            <li><a href="#">Grupos</a></li>
-            <li><a href="#">Notícias</a></li>
-          </ul>
-          <form class="navbar-form navbar-right">
-            <div class="form-group">
-              <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search"></span></button>
-              <input type="text" class="form-control" placeholder="Digite sua busca">
-              <button type="reset" class="btn btn-default"><span class="glyphicon glyphicon-remove"></span></button>
-            </div>
-          </form>
-        </div><!-- /.navbar-collapse -->
-      </div><!-- /.container-fluid -->
-    </nav>
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
 
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="js/vendor/jquery.min.js"><\/script>')</script>
-    <script src="js/bootstrap.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="js/ie10-viewport-bug-workaround.js"></script>
-  </body>
-</html>
+$response->send();
+
+$kernel->terminate($request, $response);
